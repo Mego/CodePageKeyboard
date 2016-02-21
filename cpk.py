74bc10d6db0ea9f9a1d6bab5a94644f13650aa80
Ryan Rowe <Mego@users.noreply.github.com>
2016-02-20 18:51:00 -0600

    add iso8859-1 back

--- cpk.py
+++ cpk.py
@@ -10,15 +10,17 @@ codepages = {
     "jelly":jellytable
 }
 
-supported_pages = ['ascii', 'cp037', 'cp1006', 'cp1026', 'cp1140', 'cp1250',
+supported_pages = [
+'ascii', 'cp037', 'cp1006', 'cp1026', 'cp1140', 'cp1250',
 'cp1251', 'cp1252', 'cp1253', 'cp1254', 'cp1255', 'cp1256', 'cp1257', 'cp1258',
 'cp424', 'cp437', 'cp500', 'cp720', 'cp737', 'cp775', 'cp850', 'cp852',
 'cp855', 'cp856', 'cp857', 'cp858', 'cp860', 'cp861', 'cp862', 'cp863',
-'cp864', 'cp865', 'cp866', 'cp869', 'cp874', 'cp875', 'iso8859_10',
-'iso8859_13', 'iso8859_14', 'iso8859_15', 'iso8859_16', 'iso8859_2',
-'iso8859_3', 'iso8859_4', 'iso8859_5', 'iso8859_6', 'iso8859_7', 'iso8859_8',
-'iso8859_9', 'jelly', 'koi8_r', 'koi8_u', 'mac_cyrillic', 'mac_greek',
-'mac_iceland', 'mac_latin2', 'mac_roman', 'mac_turkish', 'ptcp154']
+'cp864', 'cp865', 'cp866', 'cp869', 'cp874', 'cp875', 'iso8859_1',
+'iso8859_10', 'iso8859_13', 'iso8859_14', 'iso8859_15', 'iso8859_16',
+'iso8859_2', 'iso8859_3', 'iso8859_4', 'iso8859_5', 'iso8859_6', 'iso8859_7',
+'iso8859_8', 'iso8859_9', 'jelly', 'koi8_r', 'koi8_u', 'mac_cyrillic',
+'mac_greek', 'mac_iceland', 'mac_latin2', 'mac_roman', 'mac_turkish', 'ptcp154'
+]
 
 for enc in supported_pages:
     document["codepage"].html += '<option value="{}"{}>{}</option>'.format(
